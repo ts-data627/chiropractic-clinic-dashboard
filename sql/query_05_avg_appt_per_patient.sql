a357627a5c675bc540c8787d7343d1128c210ad7
ts-data627 <139086942+ts-data627@users.noreply.github.com>
2026-04-01 10:22:15 -0500

    Update query_05_avg_appt_per_patient.sql

--- sql/query_05_avg_appt_per_patient.sql
+++ sql/query_05_avg_appt_per_patient.sql
@@ -1,5 +1,8 @@
--- 5. Average appointments per patient
+-- Query: Average appointments per patient
 -- Business question: How often do patients return?
+-- Table: healthcare_records
+-- Author: Tevin S.
+-- Date: 2026-03
 
 SELECT COUNT(appt_id)/COUNT(DISTINCT patient_id) as avg_appts
 FROM appointments
